0e8271df9ba2a6a0ec4d15a5188460b5644622a4
Arthur Wang <arthur@hackthenorth.com>
2016-03-19 12:21:34 -0400

    created project

---
+++ Reimbursement App/Reimbursement App/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Reimbursement App
+//
+//  Created by Arthur Wang on 2016-03-19.
+//  Copyright © 2016 Hack the North. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
